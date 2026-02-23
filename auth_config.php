22ce844c87618a33ca3109dbc155319b51c71228
Abid Hasan <34217601+itzabidh@users.noreply.github.com>
2026-02-23 00:46:34 +0000

    Update auth_config.php

--- auth_config.php
+++ auth_config.php
@@ -1,5 +1,8 @@
 <?php
+// GitHub Secrets থেকে আসা ভেরিয়েবলগুলো
 define('CLIENT_ID', getenv('AZURE_CLIENT_ID'));
 define('CLIENT_SECRET', getenv('AZURE_CLIENT_SECRET'));
 define('TENANT_ID', getenv('AZURE_TENANT_ID'));
+define('USER_FLOW', getenv('AZURE_USER_FLOW')); // এটা অবশ্যই যোগ করতে হবে (উদা: B2C_1_signupsignin)
+define('REDIRECT_URI', 'https://blockaisolution.com/callback.php');
 ?>
